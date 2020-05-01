d64b2d01fd76200670c59c10908be05aae5f1e8e
Kaan Öztürk <oztrkkaan@gmail.com>
2020-05-01 19:39:48 +0300

    Fixed password hashing problem

--- Business/Concrete/AuthManager.cs
+++ Business/Concrete/AuthManager.cs
@@ -33,7 +33,7 @@ namespace Business.Concrete
                 return new ErrorDataResult<User>(Messages.UserNotFound);
             }
 
-            if(HashingHelper.VerifyPasswordHash(userForLoginDto.Password, userToCheck.PasswordHash, userToCheck.PasswordSalt))
+            if (!HashingHelper.VerifyPasswordHash(userForLoginDto.Password,userToCheck.PasswordHash,userToCheck.PasswordSalt))
             {
                 return new ErrorDataResult<User>(Messages.PasswordError);
             }
